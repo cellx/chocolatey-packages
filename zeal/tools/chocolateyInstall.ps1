8556bdf275dce5388948bccc268204aee587afea
Kilian Knoerzer <k.knoerzer@gmail.com>
2014-12-25 21:44:43 +0100

    complety new script
    after wrong installation, I have to change all files with other scripts.

--- zeal/tools/chocolateyInstall.ps1
+++ zeal/tools/chocolateyInstall.ps1
@@ -1,14 +1,25 @@
-$url= "https://bitbucket.org/jerzykozera/zeal-win32-binary-downloads/downloads/zeal-20140620.zip"
+$dest = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$is64bit = Get-ProcessorBits 64
-if ($is64bit) {
-	$zealdir = "${Env:ProgramFiles(x86)}"
-}else{
-	$zealdir = "${Env:ProgramFiles}"
-}
-Install-ChocolateyZipPackage "zeal" $url "$zealdir"
+# Add additional functions for packaging
+. $(Join-Path $dest "functions.ps1")
+
+# Install Zip
+$packageName = 'zeal'
+$url = 'https://github.com/jkozera/zeal/releases/download/20141123/zeal-20141123.zip'
+$url64 = 'hhttps://github.com/jkozera/zeal/releases/download/20141123/zeal-20141123.zip'
+$checksum = 'ecd6fcfa6d9bc0111597dc7ea3a22e54'
+$checksum64 = 'ecd6fcfa6d9bc0111597dc7ea3a22e54'
+Install-ChocolateyZipPackage "$packageName" "$url" "$dest" "$url64" -checksum $checksum -checksum64 $checksum64
+$exePath = $dest + '\zeal.exe'
+
+# Install desktop shortcut
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$desktopLink = Join-Path $desktop "$packageName.lnk"
+Install-ChocolateyShortcut -shortcutFilePath $desktopLink -targetPath $exePath
+
+# Install start menu shortcut
 $startMenu = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Programs))
-$zealStartMenuGroup = Join-Path $startMenu "Zeal"
-New-Item $zealStartMenuGroup -type directory -force
-$startMenuLink = Join-Path $zealStartMenuGroup "zeal.lnk"
-Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath "$zealdir\zeal\zeal.exe"
+$StartMenuGroup = Join-Path $startMenu "zeal"
+New-Item $StartMenuGroup -type directory -force
+$startMenuLink = Join-Path $StartMenuGroup "$packageName.lnk"
+Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath $exePath
